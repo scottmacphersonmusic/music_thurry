4d3d039fa05872225b38776869eabc563a2bd2f8
scott <scottmacphersonmusic@gmail.com>
2015-05-08 18:23:41 -0700

    Move pitch map to its own module

--- chord_changes.rb
+++ chord_changes.rb
@@ -3,44 +3,11 @@ require 'bundler/setup'
 
 require 'music_theory'
 require_relative 'pitch'
+require_relative 'giant_steps'
 
-roots = [[:B,  3],
-         [:D,  4],
-         [:G,  3],
-         [:Bf, 3],
-         [:Ef, 4],
-         [:Ef, 4],
-         [:A,  3],
-         [:D,  4],
-         [:G,  3],
-         [:Bf, 3],
-         [:Ef, 4],
-         [:Fs, 3],
-         [:B,  3],
-         [:B,  3],
-         [:F,  3],
-         [:Bf, 3],
-         [:Ef, 4],
-         [:Ef, 4],
-         [:A,  3],
-         [:D,  4],
-         [:G,  3],
-         [:G,  3],
-         [:Cs, 4],
-         [:Fs, 3],
-         [:B,  3],
-         [:B,  3],
-         [:F,  3],
-         [:Bf, 3],
-         [:Ef, 4],
-         [:Ef, 4],
-         [:Cs, 4],
-         [:Fs, 3]]
-
-
-roots.each do |item|
+PitchMap::ROOTS.each do |item|
   tone = Pitch.new item[0], item[1]
   freq = tone.frequency
-  note = MusicTheory::Note.new frequency: freq, duration: 0.33, output_file_name: 'giant_steps', distort: true
+  note = MusicTheory::Note.new frequency: freq, duration: 0.33, output_file_name: 'sin_jams', distort: true
   note.play
 end
