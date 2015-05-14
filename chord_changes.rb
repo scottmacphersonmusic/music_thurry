be1876215a4ee1cbc364fcefb32b5fb55ea791cd
scott <scottmacphersonmusic@gmail.com>
2015-05-13 17:02:12 -0700

    Evolve from primitive obsession

--- chord_changes.rb
+++ chord_changes.rb
@@ -5,9 +5,11 @@ require 'music_theory'
 require_relative 'pitch'
 require_relative 'giant_steps'
 
-PitchMap::ROOTS.each do |item|
-  tone = Pitch.new item[0], item[1]
-  freq = tone.frequency
-  note = MusicTheory::Note.new frequency: freq, duration: 0.33, output_file_name: 'sin_jams', distort: true
+
+root_pitches = GiantSteps::PitchMap.new.root_pitches
+
+root_pitches.each do |item|
+  freq = item.frequency
+  note = MusicTheory::Note.new frequency: freq, duration: 0.33, output_file_name: 'sine_jams', distort: true
   note.play
 end
