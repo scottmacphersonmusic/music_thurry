0bbcdd1d9db6e97c0112ac1f2435f1c20c278a94
scott <scottmacphersonmusic@gmail.com>
2015-05-13 18:04:22 -0700

    Harmonize roots with melody

--- chord_changes.rb
+++ chord_changes.rb
@@ -5,13 +5,15 @@ require 'music_theory'
 require_relative 'pitch'
 require_relative 'giant_steps'
 
-
 root_pitches = GiantSteps::PitchMap.new.root_pitches
-
 melody_pitches = GiantSteps::PitchMap.new.melody_pitches
 
-melody_pitches.each do |item|
-  freq = item.frequency
-  note = MusicTheory::Note.new frequency: freq, duration: 0.33, output_file_name: 'sine_jams', distort: true
-  note.play
-end
+roots = root_pitches.map { |note| MusicTheory::Note.new(frequency: note.frequency, duration: 0.33, distort: true) }
+melody = melody_pitches.map { |note| MusicTheory::Note.new(frequency: note.frequency, duration: 0.33, distort: true) }
+
+melody_samples = melody.map(&:samples).flatten
+root_samples = roots.map(&:samples).flatten
+
+harmonization = MusicTheory::Harmonize.new(melody_samples, root_samples)
+
+MusicTheory::Play.new([harmonization], { output_file_name: 'giant_steps' })
